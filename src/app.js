4d63466b125b4a62e9ed6fe0047f064252f857be
millka89 <91324039+millka89@users.noreply.github.com>
2021-11-07 16:25:03 +0100

    added geolocation to current location button

--- src/app.js
+++ src/app.js
@@ -88,3 +88,22 @@ celsiusTemp.addEventListener("click", convertToCelsius);
 
 let farenheitTemp = document.querySelector("#farenheit-temp");
 farenheitTemp.addEventListener("click", getFarenheit);
+
+//current location button - geolocation
+function retrievePosition(position) {
+  let apiKey = "33227af3de9a444d458da7784b71ec39";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(getWeatherParameters);
+}
+
+function startNavigator(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+}
+
+let button = document.querySelector("#geolocation-current");
+button.addEventListener("click", startNavigator);
+
+navigator.geolocation.getCurrentPosition(retrievePosition);
